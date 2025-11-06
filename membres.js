59fc9a7e43c3403d143c74412a01537a8c1a8e9a
GSMrA <artemixtsb@gmail.com>
2025-11-06 20:35:31 +0100

    Update membres.js

--- membres.js
+++ membres.js
@@ -2,8 +2,8 @@ const membres = [
   { image: "image1.png", pseudo: "Artemix89", desc: "Créateur de GeekSoft", contact: "artemixtsb@gmail.com" },
   { image: "image2.png", pseudo: "Mix260", desc: "Assistant HTML et CSS", contact: "mixpro2taff@gmail.com" },
   { image: "image3.png", pseudo: "Somebody", desc: "Gérant de la communication a temp partiel", contact: "Aucun Contact" },
-  { image: "image4.png", pseudo: "Stor", desc: "Tout premier client pour son serveur. Également Testeur a temp partiel et Youtuber", contact: "Son discord : https://discord.gg/cd7YWe8pqV <br>Youtube : https://www.youtube.com/@levraistor" },
-  { image: "image5.png", pseudo: "Jess_le_vadrouilleur", desc: "Deusième CLient et également amis de longue date", contact: "Son discord : https://discord.gg/hyqEucjydR" },
+  { image: "image4.png", pseudo: "Stor", desc: "Tout premier client pour son serveur. Également Testeur a temp partiel et Youtuber", contact: "Son discord : <a href=\"https://discord.gg/cd7YWe8pqV\" target=\"_blank\" rel=\"noopener noreferrer\">https://discord.gg/cd7YWe8pqV</a><br>Youtube : <a href=\"https://www.youtube.com/@levraistor\" target=\"_blank\" rel=\"noopener noreferrer\">https://www.youtube.com/@levraistor</a>" },
+  { image: "image5.png", pseudo: "Jess_le_vadrouilleur", desc: "Deusième CLient et également amis de longue date", contact: "Son discord : <a href=\"https://discord.gg/hyqEucjydR\" target=\"_blank\" rel=\"noopener noreferrer\">https://discord.gg/hyqEucjydR</a>"},
   //{ image: "image6.png", pseudo: "Ethan", desc: "Assistant technique et modérateur Discord.", contact: "ethan@geeksoft.online" }
 ];
 
